9518c3aa55c7f7e7729994dd42a28a394a80aea0
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-08-20 14:27:05 +0530

    Add project files.

---
+++ Var/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
